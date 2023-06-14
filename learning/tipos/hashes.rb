c0931d7942aa2cd39bfac8055e1da2cece2d43f2
Lucas Israel <lisraelalves92@gmail.com>
2023-06-14 16:34:49 -0300

    feat: new form for initializing hashes

--- learning/tipos/hashes.rb
+++ learning/tipos/hashes.rb
@@ -9,3 +9,18 @@ pessoa = {
 pessoa.each do |i, o|
   print "#{i}: #{o} \n" 
 end
+
+# outra forma de iniciar hash
+
+months = Hash.new "month"
+
+puts months
+puts "#{months[0]}"
+puts "#{months[72]}"
+
+# acessando hash
+
+H = Hash["a" => 100, "b" => 200]
+
+puts "#{H['a']}"
+puts "#{H['b']}"
